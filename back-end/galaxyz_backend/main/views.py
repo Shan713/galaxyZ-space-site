a89b812c80334dce88fa8ee35b784dbf4b6782f3
Shantharam P <shanthu2005best@gmail.com>
2025-06-30 23:09:36 +0530

    removed back-end comp

--- back-end/galaxyz_backend/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from users.models import Workshop, Course
-
-def home(request):
-    workshops = Workshop.objects.all().order_by('-date')
-    courses = Course.objects.all().order_by('-created_at')
-    return render(request, 'main/index.html', {'workshops': workshops, 'courses': courses})
-
